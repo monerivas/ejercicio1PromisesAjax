202896f7002d2d1c4737fba261758279d0323cd0
Mone Rivas <mone.rivas@gmail.com>
2017-07-13 17:15:10 -0500

    impresion de los 6 planetas en tarjetas de materialize

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,55 @@
 function getJSON(url) {
     return new Promise(function (resolve, reject) {
-        var ajax = new XMLHttpRequest(); // Yo qiuiero que tu, servidor http me des informacion. Es un objeto que puede ser usado para pedir data del servidor web. Se puede actualizar la pagina sin recargarla. Pide data del servidor despues de que la pagina se ha cargado. 
-        ajax.open("GET", url); // El open especifica el tipo de peticion del request (get, post, delete, patch etc.)
-        ajax.send(); // Envia el request al servidor web usando get. 
-        ajax.onreadystatechange = function () { // El onready... define una función que debe de ser llamada cuando el readystate cambie. 
-            if (ajax.readyState == 4) { //El readyState igualado a cuatro significa que el response está finalizado y listo. 
-                resolve(JSON.parse(ajax.responseText)); //El resolve acepta que la promesa se cumplió. El parse convierte a objeto el ajax. El responseText obtiene la data de la respuesta como un string. 
+        var ajax = new XMLHttpRequest(); // Yo qiuiero que tu, servidor http me des informacion. 
+        ajax.open("GET", url); // El open le dice al servidor vete preparando para hacer una peticion
+        ajax.send(); // El send envia la peticion al servidor que se estaba cocinando con el open
+        ajax.onreadystatechange = function () { // Es como un escuchador para cuando el estado cambie
+            if (ajax.readyState == 4) { //El readyState igual a 4 significa que el response está finalizado y listo. 
+                resolve(JSON.parse(ajax.responseText)); //El resolve quiere decir que la promesa se cumplió. El LSON.parse convierte el sting a un objeto JSON
             }
         }
     })
 };
 
-getJSON("data/earth-like-results.json") // Este "data..." es el parametro url de nuestra función anterior. 
-    .then(function (mensaje) {
-        return (getJSON(mensaje.results[0]))
-    }) // el return nos permite acceder a una nueva promesa ya que solo es un then por promise. 
-    .then(function (resultado) {
-        console.log(resultado)
-    }); // este then funciona para que el promise anterior sea mostrado en consola.
+
+getJSON("data/earth-like-results.json")
+    /*obtener información global deL JSON*/
+    .then(function (contenidoDelObjeto) {
+        return getJSON(contenidoDelObjeto.results.forEach(function (planetas) {
+            /*obtener información de la propiedad result deL JSON que son el conjunto de planetas*/
+            getJSON(planetas)
+            /*obtener información de cada elemento deL JSON que es el planeta individual*/
+                .then(function (planeta) {                
+                console.log(planeta);
+                funcionImpresora(planeta);
+                
+                });
+        }));
+    });
+
+function funcionImpresora(planeta){
+    var plantillFinal="";
+        plantillFinal += plantilla.replace("__tituloPlaneta__", planeta.pl_name).replace("__fecha__", planeta.pl_disc).replace("__telescopio__", planeta.pl_telescope);
+        
+        var areaMostrado = document.getElementById("contenedorPlanetas");
+        
+        areaMostrado.innerHTML+= plantillFinal;
+        
+    };
+
+
+
+var plantilla = "<div class='row'>" +
+    "<div class='col s12 m7'>" +
+    "<div class='card'>" +
+    "<div class='card-image'>" +
+    "<img src='static/img/planeta.jpg'>" +
+    "<span class='card-title'>__tituloPlaneta__</span>" +
+    "</div>" +
+    "<div class='card-content'>" +
+    "<span>__fecha__</span>" + "-"+
+    "<span>__telescopio__</span>"+ 
+    "</div>" +
+    "</div>" +
+    "</div>" +
+    "</div>";
